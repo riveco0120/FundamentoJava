95bf4a37f03b1145f3493a908534bd1db07ab0a1
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-16 11:07:44 -0500

    agregando funcionalida para calculo de numeros impares

--- src/main/java/com/sofkau/fundamentouno/ejercicios/NumeroImpar.java
+++ src/main/java/com/sofkau/fundamentouno/ejercicios/NumeroImpar.java
@@ -1,4 +1,14 @@
 package com.sofkau.fundamentouno.ejercicios;
 
 public class NumeroImpar {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <=100 ; i++) {
+
+            if((i%2!=0)){
+                System.out.println("el numero es impar: = " + i);
+            }
+
+        }
+    }
 }
